8dd47a43160e638832af30aa041c8e31de229ede
Daria D <darja.dolbe@gmail.com>
2020-07-04 16:08:23 +0300

    Basic logic

--- Form1.cs
+++ Form1.cs
@@ -16,20 +16,78 @@ namespace puzzle15
         {
             InitializeComponent();
             InitializePuzzle();
+            InitializePuzzleBlocks();
         }
 
         private void InitializePuzzle()
         {
-            this.BackColor = Color.SlateBlue;
+            this.BackColor = Color.DarkSlateBlue;
             this.Text = "Puzzle 15";
+            this.ClientSize = new Size(500, 500);
         }
 
-        private void InitializeButtons()
+        private void InitializePuzzleBlocks()
         {
-            Button button;
-            for (int i = 1; i < 16; i++)
+            int blockCount = 1;
+            PuzzleBlock block;
+            for (int row = 1; row < 5; row++)
             {
-                button = new (Button);
+                for (int col = 1; col < 5; col++)
+                {
+                    block = new PuzzleBlock();
+                    block.Top = row * 84;
+                    block.Left = col * 84;
+                    block.Text = blockCount.ToString();
+
+                    block.Click += new EventHandler(Block_Click);
+
+                    if (blockCount == 16)
+                    {
+                        block.Name = "Emptyblock";
+                        block.Text = string.Empty;
+                        block.BackColor = Color.SlateGray;
+                    }
+                    this.Controls.Add(block);
+                    blockCount++;
+                }
+
+            }
+        }
+
+        private void Block_Click (object sender, EventArgs e)
+        {
+            Button block = (Button)sender;
+            if (IsAdjanced(block))
+            {
+                SwapBlocks(block);
+            }
+        }
+
+        private void SwapBlocks(Button block)
+        {
+            Button emptyBlock = (Button)this.Controls["EmptyBlock"];
+            Point oldLocation = block.Location;
+            block.Location = emptyBlock.Location;
+            emptyBlock.Location = oldLocation;
+        }
+
+        private bool IsAdjanced (Button block)
+        {
+            double a;
+            double b;
+            double c;
+            Button emptyBlock = (Button)this.Controls["EmptyBlock"];
+            a = Math.Abs(emptyBlock.Top - block.Top);
+            b = Math.Abs(emptyBlock.Left - block.Left);
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+            if (c < 85)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
